ea4477d3e5e4b06083bab54ba20ac6bef797f1ba
Till Grallert <till.grallert@hu-berlin.de>
2026-07-21 15:32:32 +0200

    added a md dictionary of links to the output

--- src/convert-rq-to-uri.sh
+++ src/convert-rq-to-uri.sh
@@ -2,8 +2,12 @@
 # this script reads a file (SPARQL query) and URL encodes it for use as direct links
 # ask user for input directory
 read -p "Please provide a directory with files to be encoded: " -r input_dir
+cd $input_dir && pwd
 output_dir=$input_dir
 base_url="https://query.wikidata.org/embed.html#"
+# create a log file
+logfile="links_embedded.md"
+echo "" > "$logfile"
 
 for file in $input_dir/*.rq;
     do
@@ -14,6 +18,9 @@ for file in $input_dir/*.rq;
         # URL-encode the string
         query_urlencoded=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0])' "$query")
         # process the string by stripping out comments and condense whitespace
+        query_clean=$(echo "$query_urlencoded" | perl -pe 's/(%20)*%23%20.+?%0A/%20/g' | perl -pe 's/(%20)+/%20/g')
+        url="$base_url/$query_clean"
         # save with prefix as full URL
-        echo "$base_url$query_urlencoded" | perl -pe 's/(%20)*%23%20.+?%0A/%20/g' | perl -pe 's/(%20)+/%20/g' > $output_dir/$name".txt"
+        echo "$url" > $output_dir/$name".txt"
+        echo "[rq:$name]: $url" >> $logfile
     done
